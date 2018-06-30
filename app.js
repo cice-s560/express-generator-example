2db601d70b34d3800454319500919760f5b793c3
GonzaloCedillo <goncedillo@gmail.com>
2018-06-30 14:52:48 +0200

    Definiendo Jade

--- app.js
+++ app.js
@@ -1,27 +1,32 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+// Middleware de terceros para logs de peticiones HTTP
 app.use(logger('dev'));
+// Middleware integrado para manejo de JSON
 app.use(express.json());
+// Middleware integrado para entender urls
 app.use(express.urlencoded({ extended: false }));
+// Middleware de terceros para manejo de cookies
 app.use(cookieParser());
+// Middleware integrado de estáticos
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Middlewares de rutas
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
+// Middlewares de errores
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,19 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  // .render: es un método que renderiza plantillas
+  // con el motor seteado en app.set view-engine
+  res.render('home', { clase: 'S560' });
+});
+
+router.get("/clase", (req, res) => {
+  let nombres = [];
+
+  if (req.query.nombres) {
+    nombres = req.query.nombres.split(',');
+  }
+
+  return res.status(200).render("home", { nombres });
 });
 
 module.exports = router;
